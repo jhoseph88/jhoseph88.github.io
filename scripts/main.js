f18bcd843a2f79965aeeedcccc9bdfd183397d25
Cameron Barnes <ccbarnes88@gmail.com>
2016-01-02 15:39:09 -0500

    rough Conway Game of Life

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,9 @@
-var myImage = document.querySelector('img');
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
 
-myImage.onclick = function() {
-	var mySrc = myImage.getAttribute('src');
-	if (mySrc === 'images/sunset.png') {
-		myImage.setAttribute('src', 'images/sunset1.jpg');
-		myImage.width = "500";
-		myImage.height = "300";
-		myImage.style.boxShadow = "10px, 10px, 5px, blue";
-	} else {
-		myImage.setAttribute('src', 'images/sunset.png');
-	}
-}
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-	var myName = prompt('Please enter your name.');
-	localStorage.setItem('name', myName);
-	myHeading.textContent = "Mozilla is cool, " + myName;
-
-	if (!localStorage.getItem('name') ) {
-		setUserName();
-	} else {
-		var storedName = localStorage.getItem('name');
-		myHeading.textContent = "Mozilla is cool, " + storedName;
-	}
-}
-
-myButton.onclick = function() {
-	setUserName();
-}
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>Data</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
